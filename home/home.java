4c08439033287dc433c94b0891b1befab5f94d81
kyla-127 <107767149+kyla-127@users.noreply.github.com>
2024-04-18 13:38:08 -0700

    removed hashtags

--- home/home.java
+++ home/home.java
@@ -1,6 +1,1 @@
-// // JavaScript code can go here. For example, handling the search box:
-// document.querySelector('input[type="search"]').addEventListener('keypress', function(event) {
-//     if (event.key === 'Enter') {
-//         alert('Search functionality not implemented.');
-//     }
-// });
+
