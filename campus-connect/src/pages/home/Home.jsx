3ff9d3fd6bc92ea1aa57a7f95784d11d79403fee
kyla-127 <107767149+kyla-127@users.noreply.github.com>
2024-05-15 16:28:13 -0700

    added upload image on settings and Browse Items works from home page

--- campus-connect/src/pages/home/Home.jsx
+++ campus-connect/src/pages/home/Home.jsx
@@ -1,27 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Navbar from '../../components/navbar/Navbar';
-import './Home.css'
+import './Home.css';
 
 function HomePage() {
-    return (
-      <div>
-        <Navbar />
-        <main>
-          <section className="new-items">
-            <h2>New Items</h2>
-            <a href="../marketplace/marketplace.html" className="button-link">
-              <button>Browse Items</button>
-            </a>
-          </section>
-          
-          <section className="my-saved">
-            <h2>My Saved</h2>
-            <button>View Saved</button>
-          </section>
-        </main>
-      </div>
-    );
-  }
-  
-  export default HomePage;
-  
+  return (
+    <div>
+      <Navbar />
+      <main>
+        <section className="new-items">
+          <h2>New Items</h2>
+          <Link to="/marketplace" className="button-link">
+            <button>Browse Items</button>
+          </Link>
+        </section>
+        
+        <section className="my-saved">
+          <h2>My Saved</h2>
+          <button>View Saved</button>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default HomePage;
