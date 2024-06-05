3941d24ef49d235b617649bb0ddda0623d7693ca
kimkc1 <116923646+kimkc1@users.noreply.github.com>
2024-06-05 12:05:22 -0700

    handling login

--- campus-connect/src/server/server.js
+++ campus-connect/src/server/server.js
@@ -181,4 +181,31 @@ app.delete('/products/:id', async (req, res) => {
     }
 });
 
+//SAVING USERS
+const userSchema = new mongoose.Schema({
+    username: String,
+    password: String
+});
+const User = mongoose.model('User', userSchema);
+
+//User Login
+app.post('/login', async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        const user = await User.findOne({ username });
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) {
+            return res.status(401).json( { message: 'Invalid password' });
+        }
+        const token = jwt.sign({userID: user._id}, process.env.JWT_SECRET);
+        res.json({ token });
+    }
+    catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
