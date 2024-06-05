491a8025b518137c6cb7428666b8df2a1e5bde92
aali234 <aalirehan@me.com>
2024-06-05 11:45:22 -0700

    fixed getting images for marketplace

--- campus-connect/src/server/server.js
+++ campus-connect/src/server/server.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 require('dotenv').config();
+const fs = require('fs');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -39,10 +41,30 @@ const Chat = mongoose.model('Chat', chatSchema);
 
 // Get all products
 app.get('/products', async (req, res) => {
+    //console.log('get products');
     try {
         const products = await Product.find();
-        res.json(products);
+        const productsWithImages = await Promise.all(products.map(async (product) => {
+            //get image
+            const imagePath = path.join(__dirname, 'images', 'images.json');
+            const existingData = JSON.parse(fs.readFileSync(imagePath));
+            const imageData = existingData[product.id];
+
+            //update json
+            const productI = {
+                id: product.id,
+                name: product.name,
+                price: product.price,
+                description: product.description, 
+                img: imageData
+            }
+            //console.log("product: ", productI.name, "with image: ", productI.img);
+            return productI;
+
+        }));
+        res.json(productsWithImages);
     } catch (error) {
+        console.error(error.message);
         res.status(500).json({ message: error.message });
     }
 });
@@ -51,13 +73,36 @@ app.get('/products', async (req, res) => {
 app.get('/products/byId', async (req, res) => {
     try {
         const productId = req.query.id;
-        console.log("search by id for:", productId);
+        //console.log("search by id for:", productId);
         const product = await Product.findOne({ id: productId });
         if (product == null) {
             return res.status(404).json({ message: 'Product not found' });
         }
-        res.json(product);
-        console.log(product.name);
+        try{
+            const imagePath = path.join(__dirname, 'images', 'images.json');
+            const existingData = JSON.parse(fs.readFileSync(imagePath));
+            const imageData = existingData[productId];
+            const productI = {
+                id: productId,
+                name: product.name,
+                price: product.price,
+                description: product.description, 
+                img: imageData
+            }
+            res.json(productI);
+            
+            
+        }
+        catch(error)
+        {
+            console.error('Error reading image data:', error);
+        }
+
+
+
+
+        // res.json(product);
+        // console.log(product.name);
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
@@ -65,19 +110,33 @@ app.get('/products/byId', async (req, res) => {
 
 // Create a new product
 app.post('/products', async (req, res) => {
-    console.log("adding to db", req.body, req.body.description);
+    console.log("adding to db", req.body, );
     console.log('^^^^^^^^^^^^added');
     const product = new Product({
         id: req.body.id,
         name: req.body.name,
         price: req.body.price,
-        description: req.body.description, // Changed req.body.desc to req.body.description
-        img: req.body.img
-        // Add more fields as needed
+        description: req.body.desc, 
+        //img: req.body.img
+       
     });
 
     try {
         const newProduct = await product.save();
+        console.log('bd confirmed: ', newProduct);
+        const imagePath = path.join(__dirname, 'images', 'images.json');
+        const id = newProduct.id;
+        const image = req.body.img;
+        let existingData = {};
+        try {
+            existingData = JSON.parse(fs.readFileSync(imagePath));
+        } catch (error) {
+            console.error('Error reading existing image data:', error);
+        }
+        console.log("writinf")
+        existingData[id ]= image;
+        fs.writeFileSync(imagePath, JSON.stringify(existingData));
+        console.log(`Image data for product ${id} saved successfully`);
         res.status(201).json(newProduct);
     } catch (error) {
         res.status(400).json({ message: error.message });
